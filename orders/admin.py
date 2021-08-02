04c2e7912e6729a12a49551cc479c6771df2bd48
windows <penk@example.com>
2021-08-02 13:45:23 +0700

    modify admin site

--- orders/admin.py
+++ orders/admin.py
@@ -3,6 +3,10 @@ from django.contrib import admin
 from .models import Order
 
 
-@admin.register(Order)
+# @admin.register(Order)
+# Modify admin site
 class OrderAdmin(admin.ModelAdmin):
     readonly_fields = ('user', 'products', 'address', 'quantity', 'total_price', 'status', 'date_created')
+
+
+admin.site.register(Order, OrderAdmin)
