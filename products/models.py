d11c26696e3b31abcdc038deb81d0604fb14edb5
windows <penk@example.com>
2021-08-07 21:16:50 +0700

    improve product detail page

--- products/models.py
+++ products/models.py
@@ -2,6 +2,15 @@ from django.db import models
 from django.urls import reverse
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    # Show category name on admin page
+    def __str__(self):
+        return self.name
+
+
 class Product(models.Model):
     # order = models.ManyToManyField(Order)
 
@@ -10,6 +19,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=12, decimal_places=2)
     date_created = models.DateField(auto_now_add=True)
     stock = models.IntegerField(default=0)
+    category = models.ManyToManyField(Category, blank=True)
     image = models.ImageField(upload_to='products/')  # automatically upload to MEDIA_ROOT/products/
 
     # Show product name on admin page
@@ -18,13 +28,3 @@ class Product(models.Model):
 
     def get_absolute_url(self):
         return reverse("products:detail", kwargs={"pk": self.pk})
-
-
-class Category(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-
-    # Show category name on admin page
-    def __str__(self):
-        return self.name
