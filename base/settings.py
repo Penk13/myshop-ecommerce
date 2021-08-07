45fa5a4be692f8fb1675a8bac5aa9ac2ee6e1139
windows <penk@example.com>
2021-08-07 13:47:49 +0700

    improve homepage with bootstrap

--- base/settings.py
+++ base/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',  # Formatting product price (ex: 12,300)
 
     'products.apps.ProductsConfig',
     'accounts.apps.AccountsConfig',
