d37fd45ac98218b8a8fa38eab6d1a83a111c2b0c
Penk13 <72376705+Penk13@users.noreply.github.com>
2021-08-13 23:07:34 +0700

    available stock on create order page

--- orders/forms.py
+++ orders/forms.py
@@ -3,9 +3,11 @@ from django import forms
 
 class CreateOrderForm(forms.Form):
     address = forms.CharField(
+        label="<strong> Address </strong>",
         widget=forms.TextInput(attrs={'placeholder': "Where can we ship this item to you?"}),
     )
     quantity = forms.IntegerField(
+        label="<strong> Quantity </strong>",
         widget=forms.TextInput(attrs={'placeholder': 'How much do you want to buy?'}),
     )
 
@@ -14,20 +16,6 @@ class CreateOrderForm(forms.Form):
         super().__init__(*args, **kwargs)
         # Get the product so we can modify it in clean method
         self.products = product
-
-    # Get all input from user
-    # def clean(self, *args, **kwargs):
-    #     # cleaned_data = super().clean(*args, **kwargs)
-    #     product = self.products
-    #     quantity = self.cleaned_data.get("quantity")
-    #     if not isinstance(quantity, int):
-    #         raise forms.ValidationError("Please enter the correct number")
-    #     if quantity <= 0:
-    #         raise forms.ValidationError("Please enter the correct number")
-    #     if not product.available():
-    #         raise forms.ValidationError("Sorry this item is out of stock, please try again later...")
-    #     if product.price * quantity >= 999999999999:
-    #         raise forms.ValidationError("Sorry your order is very expensive, try to separate it into 2 or more orders. Thanks")
 
     def clean_quantity(self):
         product = self.products
